4fd536f0f38c470e9f3dab26307f214e6e2d7819
Ivan Tsenilov <arantirex@gmail.com>
2023-10-13 18:40:11 +0400

    feat: implement welcome section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,17 +2,61 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontSize: {
+			'h1-sm': [
+				'28px',
+				{
+					lineHeight: '32px',
+					fontWeight: 800,
+				},
+			],
+			'h2-sm': [
+				'14px',
+				{
+					lineHeight: '20px',
+					letterSpacing: '1%',
+					fontWeight: 700,
+				},
+			],
+			button: [
+				'18px',
+				{
+					lineHeight: '18px',
+					letterSpacing: '3%',
+					fontWeight: 700,
+				},
+			],
+			sm: [
+				'10px',
+				{
+					lineHeight: '10px',
+					fontWeight: 800,
+				},
+			],
+			'link-sm': [
+				'12px',
+				{
+					lineHeight: '14px',
+					fontWeight: 700,
+				},
+			],
+		},
 		screens: {
 			sm: '480px',
 			md: '768px',
 			lg: '1024px',
-			xl: '1280px'
+			xl: '1280px',
 		},
 		extend: {
 			colors: {
-				black: '#1A1A1A'
-			}
-		}
+				black: '#000000',
+				nero: '#1A1A1A',
+				orange: '#FF6633',
+			},
+			fontFamily: {
+				montserrat: ['"Montserrat"', 'sans-serif'],
+			},
+		},
 	},
-	plugins: []
+	plugins: [],
 }
